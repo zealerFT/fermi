1cbfdb21cedc358abb74c1dc4d7e8f375b037ccf
feiteng <feiteng@sunallies.com>
2017-04-14 13:19:01 +0800

    '删除runtime'

--- Application/Runtime/Data/_fields/feitengblog.f_authgroup.php
+++
@@ -1,1 +0,0 @@
-a:6:{i:0;s:2:"id";i:1;s:5:"title";i:2;s:6:"status";i:3;s:5:"rules";s:3:"_pk";s:2:"id";s:5:"_type";a:4:{s:2:"id";s:16:"int(11) unsigned";s:5:"title";s:9:"char(100)";s:6:"status";s:10:"tinyint(1)";s:5:"rules";s:4:"text";}}
--- Application/Runtime/Data/_fields/feitengblog.f_authrule.php
+++
@@ -1,1 +0,0 @@
-a:9:{i:0;s:2:"id";i:1;s:3:"pid";i:2;s:4:"name";i:3;s:5:"title";i:4;s:6:"status";i:5;s:4:"type";i:6;s:9:"condition";s:3:"_pk";s:2:"id";s:5:"_type";a:7:{s:2:"id";s:16:"int(11) unsigned";s:3:"pid";s:16:"int(11) unsigned";s:4:"name";s:8:"char(80)";s:5:"title";s:8:"char(20)";s:6:"status";s:10:"tinyint(1)";s:4:"type";s:19:"tinyint(1) unsigned";s:9:"condition";s:9:"char(100)";}}
